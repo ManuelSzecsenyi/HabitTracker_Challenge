3a80c7fc2233570479f927cf7135b383ea2be302
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2020-08-12 22:56:52 +0200

    Initial Commit

---
+++ HabitTracker_Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracker_Challenge
+//
+//  Created by Manuel Szecsenyi on 12.08.20.
+//  Copyright © 2020 Manuel Szecsenyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
